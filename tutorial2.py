6d1121469795fd5389022a570e43a14a769196e4
test <yiwen079@gmail.com>
2022-09-09 11:58:24 +0800

    Initial commit

---
+++ tutorial2.py
@@ -0,0 +1,16 @@
+# Imports
+# Import *package name* as *short name*
+import pandas as pd
+print(2+2)
+print(2-2)
+
+numeric_variable1 = 120
+heights = [1.76, 1.85, 1.43, 2.31]
+heights.append(1.65)
+def best_professor(guess):
+    return print("The best professor at CityU is Dr. Li Xinyue")
+
+best_professor(guess = "Dr.Strange")
+# todo Finish the tutorial Collin
+
+
